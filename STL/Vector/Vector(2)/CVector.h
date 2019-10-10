d1a5e6c97651ca4f09e1191701b8e041695f9d96
creatorSeraph <dbgksdl00@gmail.com>
2019-10-10 17:22:57 +0900

    벡터 구현 완료, 테스트 코드 작성 준비 arroRia와 LiveShare를 통해 작업하였음

--- STL/Vector/Vector(2)/CVector.h
+++ STL/Vector/Vector(2)/CVector.h
@@ -3,11 +3,11 @@
 template<class T>
 class CVector
 {
-private:
+private://멤버 변수
 	T* p;
 	int size;
 	int capacity;
-public:
+public://생성자
 	CVector()
 		:p(nullptr), size(0), capacity(0)
 	{
@@ -16,42 +16,69 @@ public:
 	{
 		delete[] p;
 	}
-public:
+public://멤버함수
+	//가장 뒤에 데이터 하나를 추가
 	void push_back(const T& _val)
 	{
 		if (size == capacity)//공간이 모자라다
 		{
-			//공간을 새로 만들어줘야한다
+			reserve(_Calculate_growth(size + 1));//벡터를 확장한다.
 		}
 		p[size++] = _val;
 	}
+	//가장 뒤에 데이터를 삭제
 	void pop_back()
 	{
-		size--;
+		--size;
 	}
+	//원하는 위치에 데이터를 삭제
 	void erase(int _idx)
 	{
 		for (int i = _idx; i < size - 1; ++i)
 		{
 			p[i] = p[i + 1];
 		}
-		size--;
+		--size;
 	}
+	//원하는 위치에 데이터를 삽입
 	void insert(int _idx, const T& _val)
 	{
 		if (size == capacity)//공간이 모자라다
 		{
-			//공간을 새로 만들어줘야한다
+			reserve(_Calculate_growth(size + 1));//벡터를 확장한다.
 		}
-		for (int i = _idx; i < size; ++i)
+
+		for (int i = size; i > _idx; --i)
 		{
-			p[i] = p[i + 1];
+			p[i] = p[i - 1];
 		}
 		p[_idx] = _val;
-		size++;
+		++size;
 	}
+	void resize(int _size)
+	{
+		if (_size > size)
+		{
+			if (_size > capacity)//공간이 모자라다
+			{
+				reserve(_Calculate_growth(_size));//벡터를 확장한다.
+			}
+			else
+			{
+				for (int i = size; i < _size - 1; ++i)
+				{
+					p[i] = T();
+				}
+			}
+		}
+
+		size = _size;
+	}
+
+	//벡터의 크기를 예약해둔다(재할당이 일어나지 않게 방지한다)
 	void reserve(int _capacity)
 	{
+		if (_capacity >= capacity) return;//이미 그거보다 더 큰 사이즈가 할당이 되어 있으면 무시한다.
 		T* temp = p;
 		p = new T[_capacity];
 		if (temp)
@@ -63,10 +90,21 @@ public:
 		}
 		capacity = _capacity;
 	}
-public:
+public://operator
+	//배열처럼 접근할수 있게 만들기 위한 operator
 	T& operator[](int _idx)
 	{
+		//T&가 반환형인 이유는 반환 이후의 값을 수정할수 있게 하기 위함
 		return p[_idx];
+	}
+private:
+	//내부적으로 벡터의 사이즈를 늘려야 할때
+	int _Calculate_growth(int _newSize)
+	{
+		int newCapacity = capacity + capacity / 2;//capacity * 1.5
+		if (_newSize > newCapacity)
+			return _newSize;
+		return newCapacity;
 	}
 };
 
