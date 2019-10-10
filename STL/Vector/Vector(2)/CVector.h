6a22415159c41380892bdcd45b755f34793b6211
creatorSeraph <dbgksdl00@gmail.com>
2019-10-10 17:39:29 +0900

    구현 오류 수정 및 테스트 코드 구현

--- STL/Vector/Vector(2)/CVector.h
+++ STL/Vector/Vector(2)/CVector.h
@@ -4,104 +4,108 @@ template<class T>
 class CVector
 {
 private://멤버 변수
-	T* p;
-	int size;
-	int capacity;
+	T* m_p;
+	int m_size;
+	int m_capacity;
 public://생성자
 	CVector()
-		:p(nullptr), size(0), capacity(0)
+		:m_p(nullptr), m_size(0), m_capacity(0)
 	{
 	}
 	~CVector()
 	{
-		delete[] p;
+		delete[] m_p;
 	}
 public://멤버함수
 	//가장 뒤에 데이터 하나를 추가
 	void push_back(const T& _val)
 	{
-		if (size == capacity)//공간이 모자라다
+		if (m_size == m_capacity)//공간이 모자라다
 		{
-			reserve(_Calculate_growth(size + 1));//벡터를 확장한다.
+			reserve(_Calculate_growth(m_size + 1));//벡터를 확장한다.
 		}
-		p[size++] = _val;
+		m_p[m_size++] = _val;
 	}
 	//가장 뒤에 데이터를 삭제
 	void pop_back()
 	{
-		--size;
+		--m_size;
 	}
 	//원하는 위치에 데이터를 삭제
 	void erase(int _idx)
 	{
-		for (int i = _idx; i < size - 1; ++i)
+		for (int i = _idx; i < m_size - 1; ++i)
 		{
-			p[i] = p[i + 1];
+			m_p[i] = m_p[i + 1];
 		}
-		--size;
+		--m_size;
 	}
 	//원하는 위치에 데이터를 삽입
 	void insert(int _idx, const T& _val)
 	{
-		if (size == capacity)//공간이 모자라다
+		if (m_size == m_capacity)//공간이 모자라다
 		{
-			reserve(_Calculate_growth(size + 1));//벡터를 확장한다.
+			reserve(_Calculate_growth(m_size + 1));//벡터를 확장한다.
 		}
 
-		for (int i = size; i > _idx; --i)
+		for (int i = m_size; i > _idx; --i)
 		{
-			p[i] = p[i - 1];
+			m_p[i] = m_p[i - 1];
 		}
-		p[_idx] = _val;
-		++size;
+		m_p[_idx] = _val;
+		++m_size;
 	}
 	void resize(int _size)
 	{
-		if (_size > size)
+		if (_size > m_size)
 		{
-			if (_size > capacity)//공간이 모자라다
+			if (_size > m_capacity)//공간이 모자라다
 			{
 				reserve(_Calculate_growth(_size));//벡터를 확장한다.
 			}
 			else
 			{
-				for (int i = size; i < _size - 1; ++i)
+				for (int i = m_size; i < _size - 1; ++i)
 				{
-					p[i] = T();
+					m_p[i] = T();
 				}
 			}
 		}
 
-		size = _size;
+		m_size = _size;
 	}
 
 	//벡터의 크기를 예약해둔다(재할당이 일어나지 않게 방지한다)
 	void reserve(int _capacity)
 	{
-		if (_capacity >= capacity) return;//이미 그거보다 더 큰 사이즈가 할당이 되어 있으면 무시한다.
-		T* temp = p;
-		p = new T[_capacity];
+		if (_capacity <= m_capacity) return;//이미 그거보다 더 큰 사이즈가 할당이 되어 있으면 무시한다.
+		T* temp = m_p;//기존의 배열을 임시 저장하고
+		m_p = new T[_capacity];//새로운 크기의 배열을 할당한다
 		if (temp)
 		{
-			for (int i = 0; i < size; ++i)
+			for (int i = 0; i < m_size; ++i)//기존의 데이터가 있었을경우, 그 데이터들을 복사해주고
 			{
-				temp[i] = p[i];
+				m_p[i] = temp[i];
 			}
+			delete[] temp;//기존의 데이터를 삭제한다.
 		}
-		capacity = _capacity;
+		m_capacity = _capacity;
 	}
+public://Getter
+	int size() { return m_size; }
+	int capacity() { return m_capacity; }
 public://operator
 	//배열처럼 접근할수 있게 만들기 위한 operator
 	T& operator[](int _idx)
 	{
 		//T&가 반환형인 이유는 반환 이후의 값을 수정할수 있게 하기 위함
-		return p[_idx];
+		return m_p[_idx];
 	}
 private:
 	//내부적으로 벡터의 사이즈를 늘려야 할때
 	int _Calculate_growth(int _newSize)
 	{
-		int newCapacity = capacity + capacity / 2;//capacity * 1.5
+		int newCapacity = m_capacity + m_capacity / 2;//capacity * 1.5
 		if (_newSize > newCapacity)
 			return _newSize;
 		return newCapacity;
--- STL/Vector/Vector(2)/main.cpp
+++ STL/Vector/Vector(2)/main.cpp
@@ -3,5 +3,28 @@
 
 int main()
 {
+	using namespace std;
+	CVector<int> vec;
+
+	vec.push_back(4);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(5);
+	//4,1,2,5
+
+	vec.pop_back();
+	//4,1,2
+
+	vec.insert(1, 3);
+	//4,3,1,2
+	vec.erase(2);
+	//4,3,2
+
+	for (int i = 0; i < vec.size(); ++i)//배열에서 반복문 돌리듯이 번호를 통해 접근하기
+	{
+		cout << vec[i] << "\t";
+	}
+	cout << endl;
+
 	return 0;
 }
